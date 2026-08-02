978d2a634f0b25e3254189adbd35f379b68a2727
Pasha Liashenko <zedxi236@ukr.net>
2026-08-03 00:22:56 +0300

    fix: add missing validate_seat method to Ticket model

--- planetarium/models/ticket_model.py
+++ planetarium/models/ticket_model.py
@@ -1,4 +1,6 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
+
 from planetarium.models import (
     ShowSession,
     Reservation,
@@ -22,6 +24,33 @@ class Ticket(models.Model):
     class Meta:
         unique_together = ("show_session", "row", "seat")
         ordering = ("row", "seat")
+
+    @staticmethod
+    def validate_seat(row, seat, planetarium_dome, error_to_raise):
+        for ticket_attr_value, dome_attr_name, error_name in [
+            (row, "row", "row"),
+            (seat, "seat_in_row", "seat"),
+        ]:
+            count_attrs = getattr(planetarium_dome, dome_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        error_name: f"{error_name} must be in range "
+                                    f"[1, {count_attrs}], not {ticket_attr_value}"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_seat(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"row: {self.row} - seat: {self.seat}"
