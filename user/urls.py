1ed8e774d8713a28165c07905e5bcd175503f4e3
Pasha Liashenko <zedxi236@ukr.net>
2026-08-05 22:49:17 +0300

    feat: add user registration and token authentication

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+from user.views import CreateUserView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", obtain_auth_token, name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
+
+app_name = "user"
